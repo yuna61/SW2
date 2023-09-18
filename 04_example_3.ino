824b80f04302aa2028a1fc822d8579d37758cc3a
yuna61 <134582140+yuna61@users.noreply.github.com>
2023-09-18 12:35:35 +0900

    Update 04_example_3.ino

--- 04_example_3.ino
+++ 04_example_3.ino
@@ -2,33 +2,29 @@
 unsigned int count, toggle;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(PIN_LED, OUTPUT);
-  Serial.begin(115200); 
-  while(!Serial){
-    ;
+  Serial.begin(115200); //Initialize serial port
+  while (!Serial) {
+    ; //wait for serial port to connect.
   }
   Serial.println("Hello World!");
   count = toggle = 0;
-  digitalWrite(PIN_LED, toggle);
+  digitalWrite(PIN_LED, toggle); //turn off LED.
 }
 
 void loop() {
-
-  // put your main code here, to run repeatedly:
   Serial.println(++count);
   toggle = toggle_state(toggle);
-  digitalWrite(PIN_LED,toggle);
-  delay(1000);
+  digitalWrite(PIN_LED, toggle); //update LRD status.
+  delay(1000); //wait for 1000 millisecinds
 }
 
 int toggle_state(int toggle){
-  if (toggle ==0){
-    toggle ==1;
+  if (toggle == 0) {
+    toggle = 1;
   }
-  else if (toggle==1){
-    toggle == 0;
+  else if (toggle == 1) {
+    toggle = 0;
   }
-
   return toggle;
 }
